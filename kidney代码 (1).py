b5b4017cc2906f49b1df5cabd0b873cab4cb67f5
lpzpython <lpz_python@163.com>
2025-09-08 21:22:26 +0800

    Update kidney代码 (1).py

--- kidney代码 (1).py
+++ kidney代码 (1).py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import numpy as np
 from scipy import stats
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
